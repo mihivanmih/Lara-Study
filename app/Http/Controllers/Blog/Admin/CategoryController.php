691f7e2224e43299d4638e7d0e0443c7e80a35d4
Gusb <mihivanmih@gmail.com>
2021-08-12 20:08:10 +0300

    lesson 22

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 use DB;
@@ -66,8 +67,21 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    //public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
+
+/*        $rules = [
+          'title' => 'required|min:5|max:200',
+          'slug' => 'max:200|',
+          'description' => 'string|max:500|min:3',
+          'parent_id' => 'required|integer|exists:blog_categories,id'
+        ];
+
+        $validatedData = $this->validate($request, $rules);
+        $validatedData = $request->validate($rules);*/
+
+
         $item = BlogCategory::find($id);
         if(empty($item)) {
             return back()
